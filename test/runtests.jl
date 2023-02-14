be554be5a179666f0149ff99649cae05186b6193
Christoph Plate <christoph.plate@ovgu.de>
2023-02-14 17:33:59 +0100

    added tests for ExperimentalDesign from ODEProblem

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,4 +2,5 @@ using Test
 using SafeTestsets
 
 @safetestset "Criteria API" begin include("./criteria.jl") end
+@safetestset "Experimental Design" begin include("./experimental_design.jl") end
 @safetestset "Reference results" begin include("./references.jl") end
