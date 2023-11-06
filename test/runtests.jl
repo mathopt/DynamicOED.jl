2aeef1c787acc13119ed63043906cea72b0ce817
JuliusMartensen <julius.martensen@gmail.com>
2023-11-06 09:55:59 +0100

    Remove old structure

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,3 @@
 using Test
 using SafeTestsets
-
-@safetestset "DAE" begin include("dae.jl") end
-@safetestset "Criteria API" begin include("./criteria.jl") end
-@safetestset "Experimental Design" begin include("./experimental_design.jl") end
-@safetestset "Reference results" begin include("./references.jl") end
+using Aqua
